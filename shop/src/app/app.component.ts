916639cc2e3de9abd29edf10d945712bf17f92fc
YuraSS23 <nikitsenko_yura@mail.ru>
2024-02-02 16:22:07 +0300

    setup

--- shop/src/app/app.component.ts
+++ shop/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import {ProductComponent} from "./components/pruduct/product.component";
+import {ProductComponent} from "./components/product/product.component";
 import {IProduct} from "./models/product";
 import {products as data} from "./data/products"
 
