d77ff34d6f4edc55fb3cee11a986e5907d8578db
YuraSS23 <nikitsenko_yura@mail.ru>
2024-02-06 14:01:07 +0300

    errors

--- shop/src/app/app.component.ts
+++ shop/src/app/app.component.ts
@@ -4,11 +4,13 @@ import { RouterOutlet } from '@angular/router';
 import {ProductComponent} from "./components/product/product.component";
 import {IProduct} from "./models/product";
 import {ProductService} from "./services/product-service";
+import {Observable, tap} from "rxjs";
+import {GlobalErrorComponent} from "./components/global-error/global-error.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ProductComponent],
+  imports: [CommonModule, RouterOutlet, ProductComponent, GlobalErrorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -16,13 +18,21 @@ import {ProductService} from "./services/product-service";
 export class AppComponent implements OnInit{
   title = 'my shop'
   products: IProduct[] = []
+  loading = false
+  products$: Observable<IProduct[]>
+
   constructor(private productsService: ProductService) {
   }
 
   ngOnInit(): void {
-    this.productsService.getAll().subscribe(products=>{
+    this.loading = true
+    this.products$ = this.productsService.getAll().pipe(
+      tap(()=> this.loading = false)
+    )
+    /*this.productsService.getAll().subscribe(products=>{
       this.products = products
-    })
+      this.loading = false
+    })*/
   }
 
 }
--- shop/src/app/services/product-service.ts
+++ shop/src/app/services/product-service.ts
@@ -1,22 +1,36 @@
 import {Injectable} from "@angular/core";
-import {HttpClient, HttpParams} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {HttpClient, HttpErrorResponse, HttpParams} from "@angular/common/http";
+import {catchError, delay, Observable, retry, throwError} from "rxjs";
 import {IProduct} from "../models/product";
+import {ErrorService} from "./error.service";
 
 @Injectable({
   providedIn: "root"
 })
 
 export class ProductService {
-  constructor(private http: HttpClient) {
+  constructor(
+    private http: HttpClient,
+    private errorService: ErrorService
+  ) {
   }
 
   getAll(): Observable<IProduct[]> {
-    return this.http.get<IProduct[]>("https://fakestoreapi.com/products/", {
+    return this.http.get<IProduct[]>("https://fakestoreapi.com/products", {
       params: new HttpParams({
-          fromString: "limit=5"
-        }
-      )
-    })
+        fromString: "limit=5"
+
+        //fromObject: {limit: 5}
+      })
+    }).pipe(
+      delay(2000),
+      retry(2),
+      catchError(this.errorHandler.bind(this))
+    )
+  }
+
+  private errorHandler(error: HttpErrorResponse) {
+    this.errorService.handle(error.message)
+    return throwError(() => error.message)
   }
 }
